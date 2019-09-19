f641f226a5e5e95c91785308405164a850d6b911
Kumaresan3003 <55449353+Kumaresan3003@users.noreply.github.com>
2019-09-19 13:17:30 -0700

    Task Manager DB changes
    Task Manager DB changes

---
+++ TaskManager.Database/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,11 @@
+﻿
+PRINT '1.PostDeployment.sql - START'
+GO
+
+--:r 
+GO
+
+PRINT '1.PostDeployment.sql - END'
+GO
+
+
---
+++ TaskManager.Database/dbo/Tables/Task.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Task] (
+    [Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [ParentId]  INT           NULL,
+    [Task]       NVARCHAR (100) NOT NULL,
+    [Start_Date] DATETIME      NOT NULL,
+    [End_Date]   DATETIME      NOT NULL,
+    [Priority]   INT           NOT NULL,
+    [EndTask] BIT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
